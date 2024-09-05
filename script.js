deaa6b88d8313254aa382ccd5993f0fdfd4e95c7
Abhineet Kumar <107398306+Abhineet9781@users.noreply.github.com>
2024-09-05 12:15:29 +0530

    script.js

--- script.js
+++ script.js
@@ -39,60 +39,60 @@ function playAll(songNumber){
             banner.style.backgroundImage = "url(images/b01.png)";
             bannerImage.setAttribute("src", "images/image 1.png");
             bannerHeading.textContent = "Mike Posner - I took a pill in Ibiza (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
+            bannerText.textContent = "By Abhineet Kumar";
         } else if (songNumber === 2) {
             banner.style.backgroundImage = "url(images/b02.png)";
             bannerImage.setAttribute("src", "images/image 2.png");
             bannerHeading.textContent = "Ghar (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
+            bannerText.textContent = "By Abhineet Kumar";
         }
         else if (songNumber === 3) {
             banner.style.backgroundImage = "url(images/b03.png)";
             bannerImage.setAttribute("src", "images/image 3.png");
             bannerHeading.textContent = "Kahi to hogi woh (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
+            bannerText.textContent = "By Abhineet Kumar";
         }
         else if (songNumber === 4) {
             banner.style.backgroundImage = "url(images/b04.png)";
             bannerImage.setAttribute("src", "images/image 4.jpg");
             bannerHeading.textContent = "Kasoor (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
+            bannerText.textContent = "By Abhineet Kumar";
         }
         else if (songNumber === 5) {
             banner.style.backgroundImage = "url(images/b05.png)";
             bannerImage.setAttribute("src", "images/image 5.png");
             bannerHeading.textContent = "Tu Hai Kahan";
-            bannerText.textContent = "By Ishaan Kaushik";
+            bannerText.textContent = "By Abhineet Kumar";
         }
         else if (songNumber === 6) {
             banner.style.backgroundImage = "url(images/b06.png)";
             bannerImage.setAttribute("src", "images/image 6.png");
             bannerHeading.textContent = "Baarishein - Anuv Jain (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
+            bannerText.textContent = "By Abhineet Kumar";
         }
         else if (songNumber === 7) {
             banner.style.backgroundImage = "url(images/b07.png)";
             bannerImage.setAttribute("src", "images/image 7.png");
             bannerHeading.textContent = "Waqt Ki Baatein (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
+            bannerText.textContent = "By Abhineet Kumar";
         }
         else if (songNumber === 8) {
             banner.style.backgroundImage = "url(images/b08.png)";
             bannerImage.setAttribute("src", "images/image 8.png");
             bannerHeading.textContent = "Lovely feat. Anika Vidyarthi (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
+            bannerText.textContent = "By Abhineet Kumar";
         }
         else if (songNumber === 9) {
             banner.style.backgroundImage = "url(images/b09.png)";
             bannerImage.setAttribute("src", "images/image 9.png");
             bannerHeading.textContent = "Let me down slowly - Alec Benjamin (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
+            bannerText.textContent = "By Abhineet Kumar";
         }
         else if (songNumber === 10) {
             banner.style.backgroundImage = "url(images/b010.png)";
             bannerImage.setAttribute("src", "images/image 10.png");
             bannerHeading.textContent = "Jee Le Zara - (Cover)";
-            bannerText.textContent = "By Ishaan Kaushik";
+            bannerText.textContent = "By Abhineet Kumar";
         }
     }
 }
